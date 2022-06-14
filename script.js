24f53b20301de61d55f69c2427f4049194503c88
Marcos Rodrigues <mquinhoz@hotmail.com>
2022-06-14 17:03:26 -0300

    String usando template em JS

---
+++ script.js
@@ -0,0 +1,6 @@
+let nome = "Marcos";
+let sobrenome = "Rodrigues";
+let anoNascimento = 1988;
+let anoCorrente = 2022;
+let cidade = "Porto Alegre";
+let frase= `Eu sou o ${nome} ${sobrenome} tenho ${anoCorrente - anoNascimento} anos e moro em ${cidade}`;
