4050de3d26ef8df6fad34ba23d6b721e4fa4a3f1
Marcos Rodrigues <mquinhoz@hotmail.com>
2022-06-14 17:29:10 -0300

    Adicionado console.log

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ let anoNascimento = 1988;
 let anoCorrente = 2022;
 let cidade = "Porto Alegre";
 let frase= `Eu sou o ${nome} ${sobrenome} tenho ${anoCorrente - anoNascimento} anos e moro em ${cidade}`;
+console.log (frase);
